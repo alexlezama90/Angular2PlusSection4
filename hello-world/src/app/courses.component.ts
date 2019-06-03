461ebc320709e028f57e883da622e4197ee11554
Alejandro Lezama <alexlezama90@gmail.com>
2019-06-03 15:45:53 -0500

    binding lessons

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -4,15 +4,28 @@ import { CoursesService } from './courses.service';
 @Component({
     selector: 'courses',
 
-    template: `
-        <h2>{{ title }}</h2>
-        <img src="{{ imageUrl }}"/>
+    // template: `
+    //     <h2 [textContent]="title"></h2>
+    //     <img src="{{ imageUrl }}"/>
+    // // `
+    // template: `
+    //         <h2>{{ title }}</h2>
+    //         <img [src]="imageUrl"/>
+    //         <table>
+    //             <tr>
+    //                 <td [attr.colspan]="colSpan"></td>
+    //             </tr>
+    //         </table>
+    // `
+    template:`
+            <button class="btn btn-primary">Save</button>
     `
 
 
 })
 
 export class CoursesComponent {
-    title = 'List of courses'
-    imageUrl = "http://lorempixel.com/400/200"
+    title = 'List of courses';
+    imageUrl = "http://lorempixel.com/400/200";
+    colSpan = 2;
 }
