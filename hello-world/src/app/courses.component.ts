05fa6b951cf1f227ff2b6c85c0131263979b04d9
Alejandro Lezama <alexlezama90@gmail.com>
2019-06-03 11:16:35 -0500

    introduction

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -2,32 +2,17 @@ import { Component } from '@angular/core';
 import { CoursesService } from './courses.service';
 
 @Component({
-    selector: 'courses', //<courses>
-    // template: '<h2>{{"Title: " + title}}</h2>'
-    // template: '<h2>{{getTitle()}}</h2>'
+    selector: 'courses',
+
     template: `
-        <h2>{{getTitle()}}</h2>
-        <ul>
-        <li *ngFor="let course of courses">
-        {{course}}
-        </li>
-        </ul>
-        `
+        <h2>{{ title }}</h2>
+        <img src="{{ imageUrl }}"/>
+    `
 
 
 })
 
-
 export class CoursesComponent {
     title = 'List of courses'
-    courses;
-
-    getTitle(){
-        return this.title
-    }
-
-    constructor(service: CoursesService){
-        // let service = new CoursesService(); //Tightly coupled
-        this.courses = service.getCourses();
-    }
+    imageUrl = "http://lorempixel.com/400/200"
 }
