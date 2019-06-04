84b7505867d434f8ce82f69e96de14e8b7bee04f
alexlezama90 <alexlezama90@hotmail.com>
2019-06-03 21:05:15 -0500

    event binding 101

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -17,15 +17,52 @@ import { CoursesService } from './courses.service';
     //             </tr>
     //         </table>
     // `
-    template:`
-            <button class="btn btn-primary">Save</button>
-    `
-
-
+    // template:`
+    //         <button class="btn btn-primary" [class.active] ="isActive" >Save</button>
+    // `
+    // template:`
+    //         <button [style.backgroundColor]="isActive ? 'blue' : 'white'">Save</button>
+    // `
+    // template:`
+    //     <button (click)="onSave()">Save</button>
+    // `
+    // template:`
+    //     <button (click)="onSave($event)">Save</button>
+    // `
+    // template:`
+    //     <div (click)="onDivClicked()">
+    //         <button (click)="onSave($event)">Save</button>
+    //     </div>
+    //     `
+    // template:`
+    //     <input (keyup)="onKeyUp($event)" />
+    //     `
+    template: `
+        <input (keyup.enter)="onKeyUp()" />
+        `
 })
 
 export class CoursesComponent {
-    title = 'List of courses';
-    imageUrl = "http://lorempixel.com/400/200";
-    colSpan = 2;
+    // title = 'List of courses';
+    // imageUrl = "http://lorempixel.com/400/200";
+    // colSpan = 2;
+    // isActive = false; 
+
+    //onSave(){
+    // onSave($event){
+    //     $event.stopPropagation();
+    //     console.log("Button was clicked", $event);
+    // }
+
+    // onDivClicked(){
+    //     console.log("Div was clicked");
+    // }
+
+    // onKeyUp($event) {
+    //     if ($event.keyCode === 13) console.log("ENTER was pressed");
+    // }
+
+    onKeyUp() {
+        console.log("ENTER was pressed");
+    }
 }
